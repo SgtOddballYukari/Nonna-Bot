5e5de02f3ff3d13fef7d79a3df3ef10a08f2163c
SgtOddballYukari <lelandgarcia@outlook.com>
2026-05-05 21:43:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const connectDB = require("./database/connect");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.commands = new Collection();
+
+// Load commands
+const folders = fs.readdirSync("./commands");
+
+for (const folder of folders) {
+  const files = fs.readdirSync(`./commands/${folder}`).filter(f => f.endsWith(".js"));
+
+  for (const file of files) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.data.name, command);
+  }
+}
+
+// Load events
+const eventFiles = fs.readdirSync("./events").filter(f => f.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+// DB connect
+connectDB();
+
+client.login(process.env.TOKEN);
