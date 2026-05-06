3196390e261c080758035d253604f70c54ca30ce
SgtOddballYukari <lelandgarcia@outlook.com>
2026-05-05 21:47:28 -0700

    Create interationcreate.js

---
+++ events/interationcreate.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "interactionCreate",
+  async execute(interaction, client) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    await command.execute(interaction, client);
+  }
+};
